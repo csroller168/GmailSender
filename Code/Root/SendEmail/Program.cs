2b225d9bfef034bc5962d4866e18a356a69d44ff
csroller168 <chrisshort168@gmail.com>
2022-03-18 12:19:05 -0400

    todos

--- Code/Root/SendEmail/Program.cs
+++ Code/Root/SendEmail/Program.cs
@@ -9,6 +9,7 @@ namespace SendEmail
             // TODOS
             // make a file for email body
             // make a config file for to/from address
+            // make a command line arg for number of times to send and seconds to wait in between
             var toAddress = args[0];
             var fromAddress = args[1];
             var password = args[2];
