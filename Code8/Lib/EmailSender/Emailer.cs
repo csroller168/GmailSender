151235157e7a76f8f77349c591dee9d7c88c9629
csroller168 <chrisshort168@gmail.com>
2024-05-25 22:34:48 -0400

    .net 8 migration

---
+++ Code8/Lib/EmailSender/Emailer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+
+namespace EmailSender
+{
+    public class Emailer
+    {
+        private readonly string _toAddress = "fake@fake.com"; // ConfigurationManager.AppSettings["toAddress"];
+        private readonly string _fromAddress = "fake@fake.com"; //ConfigurationManager.AppSettings["fromAddress"];
+        private readonly string _bodyFilePath = "fake@fake.com"; //ConfigurationManager.AppSettings["bodyFilePath"];
+        private readonly string _subject = "fake@fake.com"; //ConfigurationManager.AppSettings["subject"];
+
+        public void SendEmail(
+            int numIterations,
+            int numSecondsBetweenIterations,
+            string fromPassword)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress(_fromAddress);
+                message.To.Add(new MailAddress(_toAddress));
+                message.Subject = _subject;
+                message.IsBodyHtml = true; //to make message body as html  
+                message.Body = File.ReadAllText(_bodyFilePath);
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host  
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(_fromAddress, fromPassword);
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                foreach (var i in Enumerable.Range(0, numIterations))
+                {
+                    smtp.Send(message);
+                    Thread.Sleep(TimeSpan.FromSeconds(numSecondsBetweenIterations));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ Code8/Root/SendEmail/Program.cs
@@ -0,0 +1,19 @@
+﻿using EmailSender;
+
+namespace SendEmail
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var numIterations = int.Parse(args[0]);
+            var numSecondsBetweenIterations = int.Parse(args[1]);
+            var password = args[2];
+            var emailer = new Emailer();
+            emailer.SendEmail(
+                numIterations,
+                numSecondsBetweenIterations,
+                password);
+        }
+    }
+}
